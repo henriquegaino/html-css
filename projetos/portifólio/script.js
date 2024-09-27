3bd132167633d69aae47de537fa3bd238000f304
Henrique Gaino <hmgaino@gmail.com>
2024-09-27 10:14:30 -0300

    Update (portifólio)

--- projetos/portifólio/script.js
+++ projetos/portifólio/script.js
@@ -1,13 +1,21 @@
-
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i) => {
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 100 * i)
-    });
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 100 * i)
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
+
